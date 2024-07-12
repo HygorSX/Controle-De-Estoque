8b4c1fb7201ec8bfd1bc94b585f0a30c73fe31d6
Hygor <hygorlimatop123@gmail.com>
2024-07-12 16:51:50 -0300

    inicio de projeto/curso

---
+++ Controle Estoque/Controllers/GraficoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Controle_Estoque.Controllers
+{
+    public class GraficoController : Controller
+    {
+        // GET: Relatorio
+        public ActionResult PerdaMes()
+        {
+            return View();
+        }
+        public ActionResult EntradaSaidaMesa()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controle Estoque/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Controle_Estoque.MvcApplication" Language="C#" %>
